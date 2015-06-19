646db3ff89fc5e802da8e3c64a2685c8319fc4f2
alamybs <namakukingkong@gmail.com>
2015-06-19 20:34:14 +0700

    API  info

--- app/models/url.rb
+++ app/models/url.rb
@@ -7,4 +7,16 @@ class Url < ActiveRecord::Base
   def compile
     self.short = Digest::SHA1.hexdigest([self.origin, rand].join)[1..5]
   end
+  def location
+    loc  = Geokit::Geocoders::MultiGeocoder.geocode(self.infos[:ip])
+    {country: loc.country_code, state: loc.state, sity: loc.city}
+  end
+  def browser_name
+    browser = Browser.new(:ua => self.infos[:ua])
+    browser.name
+  end
+  def browser_platform
+    browser = Browser.new(:ua => self.infos[:ua])
+    browser.platform
+  end
 end
--- app/views/urls/stats.json.jbuilder
+++ app/views/urls/stats.json.jbuilder
@@ -1,1 +1,13 @@
-json.extract! @url,  :short, :origin, :infos, :created_at, :updated_at
+json.results do
+  json.origin  @url.origin
+  json.short  @url.short
+  json.infos do
+    json.ip  @url.infos[:ip]
+    json.user_agent  @url.infos[:ua]
+    json.location  @url.location
+    json.browser  @url.browser_name
+    json.platform  @url.browser_platform
+  end
+  json.created_at @url.created_at
+  json.updated_at @url.updated_at
+end
